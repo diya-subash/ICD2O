aab8aea8ac94fee7a774f0bbc1b0981b04305226
diya-subash <diya.subash@stu.ocsb.ca>
2025-09-29 03:59:34 +0000

    final 11

--- Unit1/final/js/script.js
+++ Unit1/final/js/script.js
@@ -1,11 +1,11 @@
-const button = document.getElementById('searchButton');
-const input = document.getElementById('userInput');
-button.addEventListener('click', function () {
-  const q = (input.value || '').trim();
+  const button = document.getElementById('searchButton');
+  const input = document.getElementById('userInput');
+  button.addEventListener('click', function () {
+      const q = (input.value || '').trim();
   const url = 'https://www.google.com/search?q=' + encodeURIComponent(q);
   window.open(url, '_blank');
-})
-document.getElementById('clearButton').addEventListener('click', () => {
-  document.getElementById('userInput').value = '';
+    })
+    document.getElementById('clearButton').addEventListener('click', () => {
+    document.getElementById('userInput').value = '';
   document.getElementById('userInput').focus();
-});
+    });
